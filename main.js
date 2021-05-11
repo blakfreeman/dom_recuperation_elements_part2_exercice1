1b179d53fdc899e7dc1db0ad71b46df00598b7fc
blakfreeman <61966557+blakfreeman@users.noreply.github.com>
2021-05-11 14:07:44 +0200

    start

---
+++ main.js
@@ -0,0 +1,10 @@
+//1
+
+let showBread = document.getElementsByClassName('tartine')[0];
+console.log(showBread);
+
+let idShow = document.getElementById('toast');
+console.log(idShow);
+
+let exo3 = document.getElementById('toast').lastChild;
+console.log(exo3);
